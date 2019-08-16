d7eb816e6bb20bcf7ab90c23032ebe830ba4e53f
karthik18ganesh <18karthikganesh18@gmail.com>
2019-08-17 00:14:48 +0530

    add paletteList Component and Links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelper';
 import {Route,Switch} from 'react-router-dom';
@@ -13,7 +14,7 @@ class App extends Component {
   return (
 
     <Switch>
-      <Route exact path="/" render={()=>{return <h1>Palette List</h1>}}/>
+      <Route exact path="/" render={()=><PaletteList palettes={seedColors}/>}/>
   <Route exact path="/palette/:id" render={(routeprops)=><Palette palette={generatePalette(this.findPalette(routeprops.match.params.id))}/>}/>
     </Switch>
     // <div className="App">
--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ import 'rc-slider/assets/index.css';
 import './NavBar.css'
 import { MenuItem, Snackbar, IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-
+import {Link} from 'react-router-dom';
 class NavBar extends Component {
     constructor(props){
         super(props);
@@ -31,7 +31,7 @@ class NavBar extends Component {
         return (
             <header className="Navbar">
                 <div className='logo'>
-                    <a href='#'>reactcolorpicker</a>
+                    <Link to="/">reactcolorpicker</Link>
                 </div>
                 <div className="slider-container">
                     <span>Level: {level}</span>
