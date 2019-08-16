b088cb61900b5bdc4b7d2c0a847efeb6780a5402
karthik18ganesh <18karthikganesh18@gmail.com>
2019-08-16 23:57:45 +0530

    Added route and fixed palette route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelper';
-function App() {
-  //console.log(generatePalette(seedColors[4]));
+import {Route,Switch} from 'react-router-dom';
+class App extends Component {
+  findPalette(id){
+   return seedColors.find(function(palette){
+      return palette.id === id;
+    });
+  }
+  render(){
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+
+    <Switch>
+      <Route exact path="/" render={()=>{return <h1>Palette List</h1>}}/>
+  <Route exact path="/palette/:id" render={(routeprops)=><Palette palette={generatePalette(this.findPalette(routeprops.match.params.id))}/>}/>
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
-
+}
 export default App;
